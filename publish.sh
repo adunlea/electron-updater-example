09e157b301519635b45d189f699181f6f1b007d0
Allen Dunlea <allen.dunlea@teradata.com>
2017-06-09 11:30:41 -0700

    Updates

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export GH_TOKEN="1ba8294166d3f091265a99631007ad35de40d0f4"
+
 if [ -z "$GH_TOKEN" ]; then
     echo "You must set the GH_TOKEN environment variable."
     echo "See README.md for more details."
@@ -7,4 +9,4 @@ if [ -z "$GH_TOKEN" ]; then
 fi
 
 # This will build, package and upload the app to GitHub.
-node_modules/.bin/build --win --mac -p always
+node_modules/.bin/build --win -p always
